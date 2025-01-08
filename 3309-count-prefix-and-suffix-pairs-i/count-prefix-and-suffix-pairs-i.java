6eb205c0f3a83dc4eb2cf899665ea8922e213d40
Hitarth Anand Rohra <amenu4eac21032@am.students.amrita.edu>
2025-01-08 13:00:49 +0530

    Time: 3 ms (40.04%) | Memory: 44.4 MB (18.39%) - LeetSync

---
+++ 3309-count-prefix-and-suffix-pairs-i/count-prefix-and-suffix-pairs-i.java
@@ -0,0 +1,41 @@
+class TrieNode {
+  Map<Integer, TrieNode> children = new HashMap<>();
+  int count = 0;
+}
+
+class Trie {
+  public int insert(final String word) {
+    final int n = word.length();
+    int count = 0;
+    TrieNode node = root;
+    for (int i = 0; i < n; ++i) {
+      final char prefix = word.charAt(i);
+      final char suffix = word.charAt(n - 1 - i);
+      final int key = (prefix - 'a') * 26 + (suffix - 'a');
+      node.children.putIfAbsent(key, new TrieNode());
+      node = node.children.get(key);
+      count += node.count;
+    }
+    ++node.count;
+    return count;
+  }
+
+  private TrieNode root = new TrieNode();
+}
+
+class Solution {
+  public int countPrefixSuffixPairs(String[] words) {
+    long ans = 0;
+    Trie trie = new Trie();
+
+    for (final String word : words)
+      ans += trie.insert(word);
+
+    // Cast to int and handle possible overflow if required
+    if (ans > Integer.MAX_VALUE) {
+      throw new IllegalArgumentException("Result exceeds integer range");
+    }
+
+    return (int) ans; // Cast the long result to int
+  }
+}
